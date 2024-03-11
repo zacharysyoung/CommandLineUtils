90b9e323772384f2e3cfd59bc74531e68451c007
Zach Young <zacharysyoung@gmail.com>
2024-03-11 10:52:08 -0700

    working

---
+++ pkg/temptree/temptree.go
@@ -0,0 +1,134 @@
+// Package temptree provides a simple minilanguage for creating
+// temporary file trees.
+//
+// To create a tree that contains the files f1 and f6, the empty dir d2,
+// and the dir d3, with dir d4 below that, and the empty dir d5 below that:
+//
+//	/f1
+//	/d2/
+//	/d3/d4/d5/
+//	/f6
+package temptree
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type Tree struct {
+	files []File
+
+	tempPath string
+}
+
+func NewTree(files ...File) *Tree {
+	return &Tree{files: files}
+}
+
+func (t *Tree) Debug() string {
+	var print func(File) string
+	print = func(f File) string {
+		if f.children == nil {
+			return `F("` + f.name + `")`
+		}
+
+		s := ""
+		for _, cf := range f.children {
+			s += ", " + print(cf)
+		}
+
+		return `D("` + f.name + `"` + s + `)`
+	}
+
+	s, sep := "", ""
+	for _, f := range t.files {
+		s += sep + print(f)
+		sep = ", "
+	}
+
+	return fmt.Sprintf("NewTree(%s)", s)
+}
+
+// Make creates a temp directory then recursively adds the files
+// in t underneath it.
+func (t *Tree) MakeTemp() (string, error) {
+	tempPath, err := os.MkdirTemp("", "")
+	if err != nil {
+		return "", err
+	}
+	t.tempPath = tempPath
+
+	var walk func(File, string) error
+	walk = func(n File, path string) error {
+		path = filepath.Join(path, n.name)
+
+		if n.children != nil {
+			if err := os.Mkdir(path, 0700); err != nil {
+				return err
+			}
+		} else {
+			if err := touch(path); err != nil {
+				return err
+			}
+		}
+
+		for _, nc := range n.children {
+			if err := walk(nc, path); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range t.files {
+		if err := walk(f, tempPath); err != nil {
+			return tempPath, err
+		}
+	}
+
+	return tempPath, nil
+}
+
+// touch creates an empty file.
+func touch(path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	return f.Close()
+}
+
+func (t *Tree) Remove() error { return os.RemoveAll(t.tempPath) }
+
+// File represents a file or directory in the tree.
+type File struct {
+	name     string
+	children []File
+}
+
+func F(name string) File {
+	return File{name, nil}
+}
+
+func D(name string, files ...File) File {
+	if files == nil {
+		files = []File{}
+	}
+	return File{name, files}
+}
+
+// print a compact, diagnostic string for n.
+func (f *File) print() string {
+	if f.children == nil {
+		return f.name
+	}
+
+	children, sep := "", ""
+	for _, cf := range f.children {
+		children += sep + cf.print()
+		sep = " "
+	}
+
+	return fmt.Sprintf("%s[%s]", f.name, children)
+}
---
+++ pkg/temptree/temptree_test.go
@@ -0,0 +1,113 @@
+package temptree
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestPrint(t *testing.T) {
+	for _, tc := range []struct {
+		f    File
+		want string
+	}{
+		{
+			f: D("root",
+				F("f1")),
+			want: "root[f1]",
+		},
+		{
+			f: D("root",
+				F("f1"),
+				D("d2",
+					D("d3")),
+				F("f4"),
+			),
+			want: "root[f1 d2[d3[]] f4]",
+		},
+	} {
+		if got := tc.f.print(); got != tc.want {
+			t.Errorf("%v.print() = %s; want %s", tc.f, got, tc.want)
+		}
+	}
+}
+
+func TestMakeTemp(t *testing.T) {
+	files := []File{
+		F("f1"),
+		D("d2",
+			F("f3"),
+			D("d4")),
+	}
+	wants := map[string]bool{
+		"f1":    false,
+		"d2":    true,
+		"d2/f3": false,
+		"d2/d4": true,
+	}
+
+	tree := NewTree(files...)
+	tempPath, err := tree.MakeTemp()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Assert files were added to temp folder
+	for wPath, wIsDir := range wants {
+		path := filepath.Join(tempPath, wPath)
+
+		got, err := os.Lstat(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if isDir := got.IsDir(); isDir != wIsDir {
+			t.Errorf("%s.IsDir() = %t; want %t", path, isDir, wIsDir)
+		}
+	}
+
+	// Assert only paths from files were added to temp folder
+	filepath.WalkDir(tempPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			t.Fatal(err)
+		}
+		if path == tempPath {
+			return nil
+		}
+
+		if path, err = filepath.Rel(tempPath, path); err != nil {
+			t.Fatal(err)
+		}
+		if _, ok := wants[path]; !ok {
+			t.Errorf("found path %s in temp folder, but not in wants", path)
+		}
+		return nil
+	})
+
+	if err := tree.Remove(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDebug(t *testing.T) {
+	testCases := []struct {
+		tree *Tree
+		want string
+	}{
+		{
+			tree: NewTree(F("f1"), F("f2"), F("f3")),
+			want: `NewTree(F("f1"), F("f2"), F("f3"))`,
+		},
+		{
+			tree: NewTree(F("f1"), D("d2", F("f3"), D("d4"))),
+			want: `NewTree(F("f1"), D("d2", F("f3"), D("d4")))`,
+		},
+	}
+
+	for _, tc := range testCases {
+		if got := tc.tree.Debug(); got != tc.want {
+			t.Errorf("\n%v.Debug()\n  got %s\n want %s", tc.tree.files, got, tc.want)
+		}
+	}
+}
